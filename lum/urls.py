4e162bc41ecfc334e70b1b1c0a062e5de4767831
jordotech <jordotech@gmail.com>
2016-07-13 15:53:53 -0500

    initial

---
+++ lum/urls.py
@@ -0,0 +1,15 @@
+from django.conf.urls import *
+
+from django.contrib import admin
+admin.autodiscover()
+from django.conf import settings
+urlpatterns = patterns('',
+    url(r'^$', 'lum.views.home', name='home'),
+    (r'^(?i)admin/',  include(admin.site.urls)), # admin site
+)
+
+if not settings.DEBUG:
+    # static files (images, css, javascript, etc.)
+    urlpatterns += patterns('',
+        (r'^media/(?P<path>.*)$', 'django.views.static.serve', {
+        'document_root': settings.MEDIA_ROOT}))
---
+++ settings/jordan.py
@@ -0,0 +1,31 @@
+from .base import *
+import os
+DEBUG = True
+SITE_ID = 2
+def show_toolbar(request):
+    return True
+
+DEBUG_TOOLBAR_CONFIG = {
+    'SHOW_TOOLBAR_CALLBACK': 'settings.jordan.show_toolbar',
+}
+
+INSTALLED_APPS += (
+    'debug_toolbar',
+    'template_debug',
+)
+
+
+SECRET_KEY = os.environ["SECRET_KEY_LOCAL"]
+DATABASES = {
+    'default': {
+        #'ENGINE': 'django_postgrespool',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'lum',
+        'USER': 'deploy',             # Not used with sqlite3.
+        #'USER': 'jordotech',             # Not used with sqlite3.
+        'PASSWORD': 'happy',         # Not used with sqlite3.
+        #'PASSWORD': 'Login4040',         # Not used with sqlite3.
+        'HOST': '',             # Set to empty string for localhost. Not used with sqlite3.
+        'PORT': '',             # Set to empty string for default. Not used with sqlite3.
+    }
+}
