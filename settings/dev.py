9bea74013f631cfd503b8af902d20d006821b1fa
jordotech <jordotech@gmail.com>
2016-07-14 11:45:58 -0500

    dev.py

---
+++ settings/dev.py
@@ -0,0 +1,25 @@
+from .base import *
+import os
+DEBUG = False
+SITE_ID = 2
+
+INSTALLED_APPS += (
+    'debug_toolbar',
+    'template_debug',
+)
+
+
+SECRET_KEY = os.environ["SECRET_KEY_LOCAL"]
+DATABASES = {
+    'default': {
+        #'ENGINE': 'django_postgrespool',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'lum',
+        'USER': 'deploy',             # Not used with sqlite3.
+        #'USER': 'jordotech',             # Not used with sqlite3.
+        'PASSWORD': 'happy',         # Not used with sqlite3.
+        #'PASSWORD': 'Login4040',         # Not used with sqlite3.
+        'HOST': '',             # Set to empty string for localhost. Not used with sqlite3.
+        'PORT': '',             # Set to empty string for default. Not used with sqlite3.
+    }
+}
