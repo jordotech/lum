9c4fda986518690f4afce65386a61316924d2043
Jordan Austin <jordotech@gmail.com>
2017-06-15 17:15:40 -0500

    pinging task

--- pinger/tasks.py
+++ pinger/tasks.py
@@ -1,13 +1,26 @@
 from __future__ import absolute_import
 
 from celery import shared_task, task
-
+from .models import Url
 
 import logging
 logger = logging.getLogger('lum')
 
+def email():
+    from django.core.mail import send_mail
+
+    send_mail(
+        '%s is down' % (url),
+        'Here is the message.',
+        'admin@cg-dev.com',
+        ['jordotech@gmail.com'],
+        fail_silently=False,
+    )
+import requests
 @task()
 def pinger():
-
-
+    logger.debug('in ping...')
+    for url in Url.objects.filter(active=True, stop_notifying=False):
+        response = requests.get(url.url)
+        logger.debug(response.status_code)
     return True
